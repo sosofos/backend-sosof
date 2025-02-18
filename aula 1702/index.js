f31a6b04e6f68fa2bde8fd0f64b5334ddce9fb74
sosofos <dantassofia84@gmail.com>
2025-02-18 08:15:21 -0300

    aula 2

---
+++ aula 1702/index.js
@@ -0,0 +1,44 @@
+
+class Usuario {
+    constructor(nome, email, senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha; // Atributo privado
+    }
+
+    #senha; // Declaração correta de atributo privado
+
+    autenticar(senha) {
+        return senha === this.senha;
+    }
+
+    alterarSenha(novaSenha) {
+        this.senha = novaSenha;
+        console.log('Senha alterada com sucesso');
+    }
+}
+
+// Classe Admin que herda de Usuario
+class Admin extends Usuario {
+    constructor(nome, email, senha, nivelAcesso) {
+        super(nome, email, senha); // Chama o constructor da classe pai
+        this.nivelAcesso = nivelAcesso;
+    }
+
+    banirUsuario(usuario) {
+        console.log(`${usuario.nome} foi banido pelo admin ${this.nome}`);
+    }
+
+    // Polimorfismo: Sobrescrevendo o método autenticar
+    autenticar(senha) {
+        return senha === this.senha && this.nivelAcesso === 'alto';
+    }
+}
+
+// Exemplo de uso
+const usuario1 = new Usuario('Julia', 'Miranda@gmail.com', '3536');
+const usuario2 = new Admin('Jéssica', 'Lohanny@gmail.com', '1111', 'alto');
+
+console.log(usuario1.autenticar('3536')); // true
+console.log(usuario2.autenticar('1111')); // true
+usuario2.banirUsuario(usuario1); // "Julia foi banida pelo admin Jéssica"
